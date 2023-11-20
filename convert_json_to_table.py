19fc042c5255a07276b12c0ef8962263981304a7
Ashmita Sawant <95630121+ashmitasawant@users.noreply.github.com>
2023-11-20 20:18:33 +0530

    Update convert_json_to_table.py

--- convert_json_to_table.py
+++ convert_json_to_table.py
@@ -2,55 +2,55 @@ import json
 import csv
 
 json_data = """
-{  # Paste your JSON data here
-  "@type": "EnvironmentExtensions",
-  "connections": {
-    "@type": "Connections",
-    "connection": [
-      {
-        "@type": "Connection",
-        "field": [
-          {
-            "@type": "Field",
-            "id": "authType",
-            "encryptedValueSet": false,
-            "usesEncryption": false,
-            "componentOverride": false,
-            "useDefault": true
-          },
-          {
-            "@type": "Field",
-            "id": "oauthOptions/OAuth2Config/credentials/@clientId",
-            "encryptedValueSet": false,
-            "usesEncryption": false,
-            "componentOverride": false,
-            "useDefault": true
-          },
-          {
-            "@type": "Field",
-            "id": "oauthOptions/OAuth2Config/credentials/@clientSecret",
-            "encryptedValueSet": false,
-            "usesEncryption": false,
-            "componentOverride": false,
-            "useDefault": true
-          },
-          {
-            "@type": "Field",
-            "id": "oauthOptions/OAuth2Config/credentials/@accessToken",
-            "encryptedValueSet": false,
-            "usesEncryption": true,
-            "componentOverride": false,
-            "useDefault": true
-          }
-        ],
-        "id": "4d3f80eb-5a5e-421e-b596-c5a56e739607",
-        "name": "Boomi_Service_Slack_Connector"
-      }
-    ]
-  },
-  "environmentId": "b8ece4bb-0306-45dc-8911-c31dd6e11d14",
-  "extensionGroupId": "",
-  "id": "b8ece4bb-0306-45dc-8911-c31dd6e11d14"
+{
+	"@type": "EnvironmentExtensions",
+	"connections": {
+		"@type": "Connections",
+		"connection": [
+			{
+				"@type": "Connection",
+				"field": [
+					{
+						"@type": "Field",
+						"id": "authType",
+						"encryptedValueSet": false,
+						"usesEncryption": false,
+						"componentOverride": false,
+						"useDefault": true
+					},
+					{
+						"@type": "Field",
+						"id": "oauthOptions/OAuth2Config/credentials/@clientId",
+						"encryptedValueSet": false,
+						"usesEncryption": false,
+						"componentOverride": false,
+						"useDefault": true
+					},
+					{
+						"@type": "Field",
+						"id": "oauthOptions/OAuth2Config/credentials/@clientSecret",
+						"encryptedValueSet": false,
+						"usesEncryption": false,
+						"componentOverride": false,
+						"useDefault": true
+					},
+					{
+						"@type": "Field",
+						"id": "oauthOptions/OAuth2Config/credentials/@accessToken",
+						"encryptedValueSet": false,
+						"usesEncryption": true,
+						"componentOverride": false,
+						"useDefault": true
+					}
+				],
+				"id": "4d3f80eb-5a5e-421e-b596-c5a56e739607",
+				"name": "Boomi_Service_Slack_Connector"
+			}
+		]
+	},
+	"environmentId": "b8ece4bb-0306-45dc-8911-c31dd6e11d14",
+	"extensionGroupId": "",
+	"id": "b8ece4bb-0306-45dc-8911-c31dd6e11d14"
 }
 """
 
