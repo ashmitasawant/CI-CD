98895b4caec59712640dfa854108a40ccae37d61
Ashmita Sawant <95630121+ashmitasawant@users.noreply.github.com>
2023-11-22 17:25:50 +0530

    Update convert_json_to_table.py

--- convert_json_to_table.py
+++ convert_json_to_table.py
@@ -1,40 +1,40 @@
 import requests
-import base64
-import json
-import os
 import json
 import csv
 
+def download_json(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Check for errors
+    return response.json()
 
-api_url = 'https://github.com/ashmitasawant/CI-CD/blob/main/ProcessInfo/SnapshotInitial.json'
-
-response = requests.get(api_url)
-
-# Check if the request was successful (status code 200)
-print (response)
-data = json.loads(response.read())
-
-# Extract relevant data from the JSON
-connection = data["connections"]["connection"][0]
-fields = connection["field"]
-def json_to_csv(json_file, csv_file):
-    with open(json_file, 'r') as json_input:
-        data = json.load(json_input)
-
-    # Assuming the JSON is a list of dictionaries
-    if isinstance(data, list):
-        keys = data[0].keys() if data else []
-    # Assuming the JSON is a single dictionary
-    elif isinstance(data, dict):
-        keys = data.keys()
+def json_to_csv(json_data, csv_file):
+    if isinstance(json_data, list):
+        keys = json_data[0].keys() if json_data else []
+    elif isinstance(json_data, dict):
+        keys = json_data.keys()
     else:
         raise ValueError("Invalid JSON format")
 
+    with open(csv_file, 'w', newline='') as csv_output:
+        writer = csv.DictWriter(csv_output, fieldnames=keys)
+        writer.writeheader()
 
-# Create a CSV file and write the tabular data
-with open("table.csv", mode="w", newline="") as csvfile:
-    csv_writer = csv.writer(csvfile)
-    csv_writer.writerow(["Field ID", "Encrypted Value Set", "Uses Encryption", "Component Override", "Use Default"])
+        if isinstance(json_data, list):
+            writer.writerows(json_data)
+        elif isinstance(json_data, dict):
+            writer.writerow(json_data)
 
-    for field in fields:
-        csv_writer.writerow([field["id"], field["encryptedValueSet"], field["usesEncryption"], field["componentOverride"], field["useDefault"]])
+if __name__ == "__main__":
+    # Replace 'https://example.com/data.json' with your actual URL
+    json_url =  'https://github.com/ashmitasawant/CI-CD/blob/main/ProcessInfo/SnapshotInitial.json'
+    
+    # Replace 'output.csv' with your desired CSV file name
+    csv_file_path = 'output.csv'
+
+    # Download JSON from the URL
+    json_data = download_json(json_url)
+
+    # Convert JSON to CSV
+    json_to_csv(json_data, csv_file_path)
+
+    print(f"Conversion successful. CSV file saved at {csv_file_path}")
